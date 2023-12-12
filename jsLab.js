b9109b51a770401e1a3ed7677c7b8f6372077df9
Thedylanator2277 <68658950+Thedylanator2277@users.noreply.github.com>
2023-12-12 00:21:54 +0000

    Completing the Assignment

--- jsLab.js
+++ jsLab.js
@@ -13,17 +13,26 @@ let tile9 = document.querySelector('.tile-9')
 /********* NOTE: unsure which tile is which? Be sure to reference the HTML when you need to double-check! ************/
 
 
+
 // 1. Tile 1 feels a little sad. 
 // - Update the inner HTML to show an encouraging message! 
 // - Something like "YOU GOT THIS!" or even a "You're a beautiful person!" 
 
-
+let p = document.createElement('p')
+p.innerText = 'YOU GOT THIS!'
+tile1.appendChild(p)
 
 // 2. Tile 2 has a button. 
 // - Create a click event and have the background color of tile 2 change to a shade of green.
 // - BE SURE TO SAVE THE BUTTON FIRST AS A VARIABLE.
 // - Can you see if there's a way to toggle it???
 
+let tile2Button = document.querySelector('.tile-2-button');
+
+tile2Button.addEventListener('click', function(){
+  tile2.style.backgroundColor = 'green'
+
+})
 
 // 3. What's this? A totoro? Where's his friends? 
 // - Using createElement, create a friend (an image element).
@@ -31,11 +40,15 @@ let tile9 = document.querySelector('.tile-9')
 // - Append the element to tile 3. 
 // - Need some inspiration? Go to giphy.com, search a character and choose the "stickers".
 
+let friend = document.createElement('img')
+friend.src = ("https://media0.giphy.com/media/31vamYdZV5ISQ/200w.webp?cid=ecf05e4786ewihbnwahffjkgg2cakx58pl975whvx175vjvc&ep=v1_stickers_search&rid=200w.webp&ct=s")
+tile3.appendChild(friend)
 
 // 4. Oh no! A broken image! We need to fix this.
 // - Set the src for tile 4 to have an image of your favorite TV show.
 
-
+let tvShow = tile4
+tvShow.src = ("https://media1.giphy.com/media/3o7abKhOpu0NwenH3O/giphy.gif?cid=ecf05e47c1a8d")
 
 
 // These following tasks may require a little bit of digging. 
@@ -47,19 +60,28 @@ let tile9 = document.querySelector('.tile-9')
 // 5. Hmm. Looks like the font is too small. 
 // - Update the font-size of tile 5 to 35 pixels.
 
+tile5.style.fontSize = '35px'
 
 // 6. Another sad box! 
 // - Let make this tile 6 feel special and add a gradient background! 
 // - What kind of gradient? What color? YOU CHOOSE!
 
+tile6.style.backgroundImage = 'linear-gradient(to right, blue, green)' ;
+
 // 7. Too many squares. 
 // - Let's change tile 7 to have rounded edges - be sure to search for which CSS property does this :)
+tile7.style.borderRadius = '50%'
 
 // 8. Replace the image with your favorite emoji. 
 // - Try going to this site to search: https://emojipedia.org/
 
+tile8.src = ("https://media3.giphy.com/media/WtOkaikiwaR87ZvAFH/200w.webp?cid=ecf05e470hlmxfl0lk4k22urg4if1irn9mu6lw0otmtc5fs8&ep=v1_gifs_search&rid=200w.webp&ct=g")
+
 // 9. Display a random number in the tile when the button is clicked.
 // - BE SURE TO SAVE THE BUTTON FIRST AS A VARIABLE.
+let btn = document.querySelector(".random-button")
 
 
-
+btn.addEventListener('click',function(){
+  btn.innerHTML = Math.random()*20;
+})
